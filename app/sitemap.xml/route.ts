be4611971a763c5245262245ecf4cae246879f68
Kuncarlos <kuncarlos@hotmail.com>
2025-12-12 21:41:58 +0100

    Add sitemap.xml route

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,15 @@
+export async function GET() {
+  const base = "https://djwillyhomes.com";
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>${base}/</loc></url>
+  <url><loc>${base}/contact</loc></url>
+</urlset>`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=0, must-revalidate",
+    },
+  });
+}
